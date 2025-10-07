f792a9b6d6a3a951422db706392bd408691b7a45
Sumi <194182518+yt1092@users.noreply.github.com>
2025-10-07 19:42:48 +0900

    Deploy Flutter Web

--- flutter1/helloworld/android/app/src/main/kotlin/com/example/helloworld/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.helloworld
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6b0bb553213223133a1d6cdef9671841",
+"flutter_bootstrap.js": "8afced9d0a5267046979230b8615435b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
